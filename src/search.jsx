34135ac0a0d22a6a6163306b4b7486635f37b589
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-02-01 12:03:05 +0300

    search functionality

--- src/search.jsx
+++ src/search.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import JsonData from "./MOCK_DATA.json";
 
 function Search() {
 
+// the state represents anything that we write on the input field
 
-
+    const [searchterm, setsearchterm] = useState(" ")
 
 
 
@@ -15,10 +16,25 @@ function Search() {
       <div className="search">
 
 
-        <input type="text" placeholder="search..." />
-        {JsonData.map((val, key)=>{
+        <input type="text" placeholder="search..." onChange={(event) =>{setsearchterm(event.target.value)}}/>
 
-          return <div><p>{val.first_name}</p></div>
+        
+        {JsonData.filter((val)=>{
+        if(searchterm ==''){
+          return val
+        }
+        else if(val.first_name.toLowerCase().includes(searchterm.toLowerCase())){
+
+          return val
+
+        }
+          }).map((val, key)=>{
+
+          return (
+          <div className = 'names' key = {key}>
+            <p>{val.first_name}</p>
+            </div>
+          )
 
         })}
 
@@ -40,3 +56,5 @@ function Search() {
   
 
 export default Search
+
+
