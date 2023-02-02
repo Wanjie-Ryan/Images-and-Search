56f63476faa5b5f7a13060458e452b961778cf64
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-02-02 05:57:32 +0300

    final

--- src/search.jsx
+++ src/search.jsx
@@ -1,60 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import JsonData from "./MOCK_DATA.json";
 
 function Search() {
+  // the state represents anything that we write on the input field
 
-// the state represents anything that we write on the input field
-
-    const [searchterm, setsearchterm] = useState(" ")
-
-
+  const [searchterm, setsearchterm] = useState(" ");
 
   return (
     <>
-    
-
       <div className="search">
+        <input
+          type="text"
+          placeholder="search..."
+          onChange={(event) => {
+            setsearchterm(event.target.value);
+          }}
+        />
 
-
-        <input type="text" placeholder="search..." onChange={(event) =>{setsearchterm(event.target.value)}}/>
-
-        
-        {JsonData.filter((val)=>{
-        if(searchterm ==''){
-          return val
-        }
-        else if(val.first_name.toLowerCase().includes(searchterm.toLowerCase())){
-
-          return val
-
-        }
-          }).map((val, key)=>{
-
+        {JsonData.filter((val) => {
+          if (searchterm == "") {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(searchterm.toLowerCase())
+          ) {
+            return val;
+          }
+        }).map((val, key) => {
           return (
-          <div className = 'names' key = {key}>
-            <p>{val.first_name}</p>
+            <div className="names" key={key}>
+              <p>{val.first_name}</p>
             </div>
-          )
-
+          );
         })}
-
-
-
-
       </div>
-    
-
-
-
     </>
   );
 }
 
-
-
-    
-  
-
-export default Search
-
-
+export default Search;
